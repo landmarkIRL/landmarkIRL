18a2997b1280c57e518983f137029d032711ae1d
David Umoru <umorudavido@gmail.com>
2024-11-14 15:38:31 +0100

    Add GitHub Actions workflow for deployment and update site URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://landmarkirl.brimble.app"
+    site: "https://landmarkirl.xyz"
 });
