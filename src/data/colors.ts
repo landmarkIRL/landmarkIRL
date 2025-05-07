f0fd6640b712cdce3a9c6c9f6aa21ea328a9a96b
David Umoru <umorudavido@gmail.com>
2025-05-07 22:29:21 +0100

    Add color definitions and theme color type

---
+++ src/data/colors.ts
@@ -0,0 +1,61 @@
+type Color = {
+  id: string;
+  name: string;
+  hex: string;
+  forText?: boolean;
+};
+
+export type ThemeColor =
+  | "purple"
+  | "pink"
+  | "blue"
+  | "yellow"
+  | "green"
+  | "brown"
+  | "black"
+  | "white";
+
+export const colors: Record<ThemeColor, Color> = {
+  purple: {
+    id: "purple",
+    name: "Pastel Purple",
+    hex: "#CDA5EF",
+  },
+  pink: {
+    id: "pink",
+    name: "Pastel Pink",
+    hex: "#ECADD4",
+  },
+  blue: {
+    id: "blue",
+    name: "Pastel Blue",
+    hex: "#96C7F2",
+  },
+  yellow: {
+    id: "yellow",
+    name: "Pastel Yellow",
+    hex: "#ECDD85",
+  },
+  green: {
+    id: "green",
+    name: "Pastel Green",
+    hex: "#97CF9C",
+  },
+  brown: {
+    id: "brown",
+    name: "Pastel Brown",
+    hex: "#DDB896",
+  },
+  black: {
+    id: "black",
+    name: "Photocopy Black",
+    hex: "#111111",
+    forText: true,
+  },
+  white: {
+    id: "white",
+    name: "Photocopy White",
+    hex: "#E1E1E1",
+    forText: true,
+  },
+};
