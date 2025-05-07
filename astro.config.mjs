b3d667202b43213f19286eac8585d6cbd220a4f6
David Umoru <umorudavido@gmail.com>
2025-05-07 18:28:01 +0100

    Enhance Astro configuration with PostCSS plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,33 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
+import autoprefixer from "autoprefixer";
+import cssnano from "cssnano";
+import postcssClamp from "postcss-clamp";
+import postcssMediaMinMax from "postcss-media-minmax";
+import postcssUtopia from "postcss-utopia";
+import postcssLogicalViewportUnits from "@csstools/postcss-logical-viewport-units";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://landmarkirl.xyz",
-  integrations: [react()]
+  integrations: [react()],
+  prefetch: true,
+  vite: {
+    css: {
+      postcss: {
+        plugins: [
+          autoprefixer(),
+          postcssUtopia({
+            minWidth: 320,
+            maxWidth: 1240,
+          }),
+          postcssMediaMinMax(),
+          postcssLogicalViewportUnits(),
+          postcssClamp(),
+          cssnano(),
+        ],
+      },
+    },
+  },
 });
